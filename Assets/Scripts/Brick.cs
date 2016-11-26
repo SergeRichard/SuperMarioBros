1ad1ef1c89545279c9e955e71f88f7bcc913e9b7
SergeRichard <serge.richard@rogers.com>
2016-11-26 19:58:32 -0400

    Added Question mark animation with coin. Also brick move without coin.

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	Animator theAnimator;
+
+	// Use this for initialization
+	void Start () {
+		theAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player" /*&& theAnimator.GetBool("HitAnimation") == false*/) {
+			theAnimator.Play ("Idle");
+			theAnimator.SetBool ("HitAnimation",true);
+		}
+
+	}
+	public void HitNoCoinDone() {
+		theAnimator.SetBool ("HitAnimation",false);
+
+	}
+}
---
+++ Assets/Scripts/QuestionMark.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestionMark : MonoBehaviour {
+
+	Animator theAnimator;
+
+	// Use this for initialization
+	void Start () {
+		theAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			theAnimator.SetTrigger ("Hit");
+		}
+
+	}
+}
