fbe9477194680684964293c54e30808dd607911e
SergeRichard <serge.richard@rogers.com>
2016-11-28 20:41:44 -0400

    Implemented mushroom animation after hitting select question mark tile.

--- Assets/Scripts/QuestionMark.cs
+++ Assets/Scripts/QuestionMark.cs
@@ -5,6 +5,12 @@ public class QuestionMark : MonoBehaviour {
 
 	Animator theAnimator;
 
+	public enum Prize {Coin, MushroomOrFlower}
+
+	public Prize prize = Prize.Coin; 
+
+	private bool hitLeft = true;
+
 	// Use this for initialization
 	void Start () {
 		theAnimator = GetComponent<Animator> ();
@@ -15,8 +21,14 @@ public class QuestionMark : MonoBehaviour {
 		
 	}
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
-			theAnimator.SetTrigger ("Hit");
+		if (other.tag == "Player" && hitLeft) {
+			if (prize == Prize.Coin) {
+				theAnimator.SetTrigger ("Hit");
+			} else {
+				theAnimator.SetTrigger ("HitMushroom");
+
+			}
+			hitLeft = false;
 		}
 
 	}
