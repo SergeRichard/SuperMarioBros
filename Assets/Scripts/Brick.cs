c476086bc6425ecdc9d35536e91ed8dc8b67d77a
SergeRichard <serge.richard@rogers.com>
2017-04-26 22:03:31 -0300

    Started brick animation with coins.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,6 +5,7 @@ public class Brick : MonoBehaviour {
 
 	Animator theAnimator;
 	public AudioSource BumpAudioSource; 
+	public int coinAmount;
 
 	// Use this for initialization
 	void Start () {
@@ -16,15 +17,32 @@ public class Brick : MonoBehaviour {
 	
 	}
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player" /*&& theAnimator.GetBool("HitAnimation") == false*/) {
+		if (other.tag == "Player") {
 			theAnimator.Play ("Idle");
 			theAnimator.SetBool ("HitAnimation",true);
+				
 			BumpAudioSource.Play ();
 		}
+
+	}
+	void OnTriggerExit2D(Collider2D other) {
+		if (coinAmount > 0) {
+			coinAmount--;
+			theAnimator.SetInteger ("Coins", coinAmount);
+			theAnimator.SetBool ("HasOrHadCoins", true);
+		}
+
 
 	}
 	public void HitNoCoinDone() {
 		theAnimator.SetBool ("HitAnimation",false);
 
+	}
+	public void HitLastCoinDone() {
+		theAnimator.SetBool ("HitAnimation", false);
+
+	}
+	public void HitCoinsDone() {
+		theAnimator.SetBool ("HitAnimation", false);
 	}
 }
