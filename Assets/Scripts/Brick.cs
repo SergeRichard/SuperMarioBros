077a3d4cc59c04904b3b0347f2e14313bf27a933
SergeRichard <serge.richard@rogers.com>
2017-04-28 21:08:59 -0300

    Implemented brick with coins fully.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,45 +4,71 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	Animator theAnimator;
+	public Sprite MetalFace;
+	public SpriteRenderer Block;
 	public AudioSource BumpAudioSource; 
+	public AudioSource CoinSound;
 	public int coinAmount;
+	public bool isMetalFace;
+	bool triggered = false;
 
 	// Use this for initialization
 	void Start () {
 		theAnimator = GetComponent<Animator> ();
+		isMetalFace = false;
+
+		if (coinAmount > 0) {
+			theAnimator.SetBool ("HadCoins",true);
+
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+//		if (Block.sprite != MetalFace && theAnimator.GetBool ("HadCoins")==true) {
+//			isMetalFace = true;
+//			Block.sprite = MetalFace;
+//			Debug.Log ("coinAmountZero!");
+//		}
 	}
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
-			theAnimator.Play ("Idle");
+		if (coinAmount > 0) {
+			CoinSound.Play ();
+
+		}
+		BumpAudioSource.Play ();
+		if (other.tag == "Player" && triggered == false) {
 			theAnimator.SetBool ("HitAnimation",true);
-				
-			BumpAudioSource.Play ();
+			triggered = true;
+			if (!(theAnimator.GetBool ("HadCoins") == true && coinAmount <= 0)) {				
+				theAnimator.Play ("Idle");
+				theAnimator.SetBool ("HitAnimation", true);
+			}
+
 		}
 
 	}
 	void OnTriggerExit2D(Collider2D other) {
-		if (coinAmount > 0) {
+		if (coinAmount > 0 && triggered) {
+			triggered = false;
 			coinAmount--;
 			theAnimator.SetInteger ("Coins", coinAmount);
-			theAnimator.SetBool ("HasOrHadCoins", true);
+			theAnimator.SetBool ("HadCoins", true);
 		}
-
-
 	}
 	public void HitNoCoinDone() {
 		theAnimator.SetBool ("HitAnimation",false);
+		triggered = false;
 
 	}
 	public void HitLastCoinDone() {
 		theAnimator.SetBool ("HitAnimation", false);
 
+
 	}
 	public void HitCoinsDone() {
 		theAnimator.SetBool ("HitAnimation", false);
+		triggered = false;
 	}
 }
