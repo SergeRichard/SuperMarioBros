ffe5414cf5fa16474420e7586983eb7a378ae957
SergeRichard <serge.richard@rogers.com>
2017-05-29 20:58:06 -0300

    Implemented splash screen before level and after mario dies.

---
+++ Assets/ChangeLevelCounterCustom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevelCounterCustom : MonoBehaviour {
+	AudioSource DieMusicAudio;
+	public string LevelToLoad;
+	public float TimeToLoad;
+
+	// Use this for initialization
+	void Start () {
+
+		Invoke("LoadLevel", TimeToLoad);
+	}
+
+	void LoadLevel() {
+		SceneManager.LoadScene (LevelToLoad);
+
+	}
+}
---
+++ Assets/Scripts/LevelChangeCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangeCounter : MonoBehaviour {
+
+	AudioSource DieMusicAudio;
+	public string LevelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		DieMusicAudio = GetComponent<AudioSource> ();
+
+		Invoke("LoadLevel", DieMusicAudio.clip.length);
+	}
+	
+	void LoadLevel() {
+		SceneManager.LoadScene (LevelToLoad);
+
+	}
+}
